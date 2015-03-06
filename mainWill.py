766ac818be97f95f8a5ae0f48ace5dae33c41f5d
William Chou <wtchou712@gmail.com>
2015-03-05 20:17:06 -0600

    transform to lactose free

--- mainWill.py
+++ mainWill.py
@@ -5,6 +5,13 @@ import requests
 import json
 from collections import OrderedDict
 from parseRecipe import parseRecipe
+from transformation import toLactoseFree
 
-link = raw_input('Copy an allrecipes link here\n')
-parseRecipe(link)
+
+# link = raw_input('Copy an allrecipes link here\n')
+# parseRecipe(link)
+
+print "1. Lactose-free"
+print "2. insert"
+choice = raw_input('Select a transformation by number\n')
+toLactoseFree()
